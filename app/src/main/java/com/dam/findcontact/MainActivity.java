1f9832c558a28ea3f2cf1eab4740b45f23c9887e
CharbelBijani <charbel.bijani@gmail.com>
2022-06-27 23:20:14 +0200

    27/06/2022 23:20

--- app/src/main/java/com/dam/findcontact/MainActivity.java
+++ app/src/main/java/com/dam/findcontact/MainActivity.java
@@ -2,17 +2,31 @@ package com.dam.findcontact;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     Button btnFind;
-    TextView number;
+    TextView number, tvName;
+    ImageView image;
+    Bitmap photo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +35,92 @@ public class MainActivity extends AppCompatActivity {
 
         btnFind = findViewById(R.id.btnFind);
         number = findViewById(R.id.editTextPhone);
+        tvName = findViewById(R.id.tvName);
+        image = findViewById(R.id.ivImage);
 
-        Uri contentUri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_FILTER_URI, Uri.encode(number.toString()));
-        Uri contentUri2 = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number.toString()));
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvName.setText(getContactNameByPhoneNumber(MainActivity.this, number.getText().toString()));
+                photo = getDisplayPhoto(MainActivity.this, number.getText().toString());
+                image.setImageBitmap(photo);
 
-        Log.i(TAG, "onCreate: contacts = " + contentUri);
-        Log.i(TAG, "onCreate: phonelookup = " + contentUri2);
-
+            }
+        });
     }
 
-    Uri filterUri = Uri.withAppendedPath(Phone.CONTENT_FILTER_URI, Uri.encode(searchString));
-    String[] projection = new String[]{ Phone.CONTACT_ID, Phone.DISPLAY_NAME, Phone.NUMBER };
-    Cursor cur = getContentResolver().query(filterUri, projection, null, null, null);
+    public static String getContactNameByPhoneNumber(Context context, String phoneNumber) {
+        ContentResolver cr = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        Cursor cursor = cr.query(uri, new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME}, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        String contactName = null;
+        if(cursor.moveToFirst()) {
+            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+        }
 
-    
+        if(cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+
+        return contactName;
+    }
+
+    public static Bitmap getDisplayPhoto(Context context, String contactNumber) {
+
+        contactNumber = Uri.encode(contactNumber);
+        int phoneContactID = -1;
+        Cursor contactLookupCursor = context.getContentResolver().query(Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, contactNumber),
+                new String[] { ContactsContract.PhoneLookup.DISPLAY_NAME, ContactsContract.PhoneLookup._ID }, null, null, null);
+        while (contactLookupCursor.moveToNext()) {
+            phoneContactID = contactLookupCursor.getInt(contactLookupCursor.getColumnIndexOrThrow(ContactsContract.PhoneLookup._ID));
+        }
+        contactLookupCursor.close();
+
+        Bitmap photo = null;
+        if (phoneContactID != -1) {
+            Uri contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, phoneContactID);
+            Uri displayPhotoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.DISPLAY_PHOTO);
+            try {
+                AssetFileDescriptor fd = context.getContentResolver().openAssetFileDescriptor(displayPhotoUri, "r");
+
+                photo = BitmapFactory.decodeFileDescriptor(fd.getFileDescriptor());
+            } catch (IOException e) {
+            }
+        }
+
+        return photo;
+    }
+
+//    public static Bitmap fetchContactPhotoFromPhoneNumber(Context context,
+//                                                          String phoneNumber) {
+//        long id = fetchContactIdFromPhoneNumber(context, phoneNumber);
+//        if (id == 0)
+//            return null;
+//        else {
+//            Uri uri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, id);
+//            InputStream input = ContactsContract.Contacts.openContactPhotoInputStream(
+//                    context.getContentResolver(), uri);
+//            return input == null ? null : BitmapFactory.decodeStream(input);
+//        }
+//    }
+//
+//    public static long fetchContactIdFromPhoneNumber(Context context,
+//                                                     String phoneNumber) {
+//        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+//                Uri.encode(phoneNumber));
+//        Cursor cursor = context.getContentResolver().query(uri,
+//                new String[] { ContactsContract.PhoneLookup._ID }, null, null, null);
+//
+//        long id = 0L;
+//        if (cursor.moveToFirst()) {
+//            id = cursor.getLong(cursor.getColumnIndex(ContactsContract.PhoneLookup._ID));
+//        }
+//        cursor.close();
+//
+//        return id;
+//
+//    }
 }
